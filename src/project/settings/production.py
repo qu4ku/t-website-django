b527a446ec7f1f7cbf9fe2edc68ae14dbe21d6fc
qu4ku <qu4ku@hotmail.com>
2019-01-03 08:50:42 +0100

    init setup

---
+++ src/project/settings/production.py
@@ -0,0 +1,22 @@
+from .base import *
+import os
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+SECRET_KEY = os.environ.get('SECRET_KEY'),
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('NAME'),
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
+MEDIA_ROOT = '/var/www/trenerindywidualnyonline/media/'
+STATIC_ROOT = '/var/www/trenerindywidualnyonline/static/'
